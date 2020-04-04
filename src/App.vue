8ab0f41c8a66c007e8aac3305a647ada13ce9c1b
everlastlucas <everlastlucas@outlook.com>
2020-04-04 11:59:11 +0800

    add mourn

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,26 @@ import PageFooter from '@/components/PageFooter.vue';
       return {
         current: ['mail'],
       };
+    },
+    mounted() {
+      const nextday = new Date('2020-04-05 00:00:00');
+      const today = new Date();
+      if (today < nextday) {
+        const style = document.createElement('style');
+        style.innerText = `
+html {
+  filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+  filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
+  -webkit-filter: grayscale(1);
+}
+`;
+        document.head.appendChild(style);
+      }
     }
   };
 </script>
